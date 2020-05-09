26ba47c73266854f5cb0425ad16626122073c645
Ryo Yamashita <qryxip@gmail.com>
2020-05-09 15:26:35 +0900

    Define `AsDeref` and `AsDerefMut`

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
+#![no_std]
 
+pub trait AsDeref {
+    type Target;
+    fn as_deref(self) -> Self::Target;
+}
+
+pub trait AsDerefMut {
+    type Target;
+    fn as_deref_mut(self) -> Self::Target;
+}
